26cf0757cd9db4b49754676b421a9f6cee18245a
victorcals <victorcals78@gmail.com>
2023-04-26 09:31:28 -0300

    Added mary's api | removed the api TheMovie

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,58 +8,85 @@ import Keys from "../../Config/key";
 
 function Home() {
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'fab9af3022mshcd4e9c52f9e1a85p195239jsne2e03521e6ff',
-            'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
-        }
-    };
 
 
-    const [movies, setMovies] = useState([])
-
-    const image_path = 'https://image.tmdb.org/t/p/w500'
-
+    const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        fetch('https://moviesminidatabase.p.rapidapi.com/movie/order/byPopularity/', options)
+        fetch('https://my-json-server.typicode.com/marycamila184/movies/movies')
             .then(response => response.json())
-            .then(data => setMovies(data.results))
+            .then(data => setMovies(data))
+            .catch(error => {
+                console.error('Erro ao carregar filmes:', error);
+            });
     }, []);
 
     return (
-
-
-
-        < Container >
-            <h1> Movies</h1>
-
+        <Container>
+            <h1>Filmes</h1>
             <MovieList>
-
-
-
-                {
-                    movies.map(movie => {
-                        return (
-                            <Movie>
-                                <a href="https://uiverse.io/challenges/open-source-card-challenge">
-
-                                    <img src={movie.image_url} alt={movie.title} /></a>
-
-
-                                <span> {movie.title}</span>
-
-                            </Movie>
-                        )
-                    })
-                }
-
-
+                {movies && movies.map(movie => {
+                    return (
+                        <Movie key={movie.id}>
+                            <a href="https://uiverse.io/challenges/open-source-card-challenge">
+                                <img src={`${movie.poster}`} alt={movie.titulo} />
+                            </a>
+                            <span>{movie.titulo}</span>
+                        </Movie>
+                    );
+                })}
             </MovieList>
-
         </Container>
-    )
+    );
+
+
 }
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// return (
+
+
+
+//     < Container >
+//         <h1> Movies</h1>
+
+//         <MovieList>
+
+
+
+//             {
+//                 movies.map(movie => {
+//                     return (
+//                         <Movie>
+//                             <a href="https://uiverse.io/challenges/open-source-card-challenge">
+
+//                                 <img src={movie.image_url} alt={movie.title} /></a>
+
+
+//                             <span> {movie.title}</span>
+
+//                         </Movie>
+//                     )
+//                 })
+//             }
+
+
+//         </MovieList>
+
+//     </Container>
+// )
