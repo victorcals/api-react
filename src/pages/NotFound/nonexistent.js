903e720441ae7e5a99ddcbae06e6ac42e40a22e2
guilhermeandrade02 <guilhermeandrade02@hotmail.com>
2023-05-01 14:20:31 -0300

    Ajustes

--- src/pages/NotFound/nonexistent.js
+++ src/pages/NotFound/nonexistent.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-import NotFound from './nonexistent';
-import Navbar from '../../components/Headers/navbar';
 
 function nonexistent() {
-
     return (
-        < div class="container" >
+        < div className="container" >
             <h1>Pagina não encontrada</h1>
         </div >
     )
